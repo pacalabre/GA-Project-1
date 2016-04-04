e98a5c74a93f5133c78e3ec49116f24d958337e4
Paul <pacalabre@gmail.com>
2016-04-04 10:46:16 -0700

    bug: compare function only tests the first index of each array against each other

--- main.js
+++ main.js
@@ -17,8 +17,8 @@ document.getElementById('b1').addEventListener("click", function(event) {
     console.log("computer pattern ="+computerPattern);
     } else {
       playerPattern.push('b1');
+      console.log("player plays back number = "+playerPattern);
       comparePattern();
-      console.log("playerPattern ="+playerPattern);
     }
 });
 
@@ -29,8 +29,8 @@ document.getElementById('b2').addEventListener("click", function(event) {
     console.log("computer pattern ="+computerPattern);
     } else {
       playerPattern.push('b2');
+      console.log("player plays back number = "+playerPattern);
       comparePattern();
-      console.log("playerPattern ="+playerPattern);
     }
 });
 
@@ -41,8 +41,8 @@ document.getElementById('b3').addEventListener("click", function(event) {
     console.log("computer pattern ="+computerPattern);
     } else {
       playerPattern.push('b3');
+      console.log("player plays back number = "+playerPattern);
       comparePattern();
-      console.log("playerPattern ="+playerPattern);
     }
 });
 
@@ -53,8 +53,8 @@ document.getElementById('b4').addEventListener("click", function(event) {
     console.log("computer pattern ="+computerPattern);
     } else {
       playerPattern.push('b4');
+      console.log("player plays back number = "+playerPattern);
       comparePattern();
-      console.log("playerPattern ="+playerPattern);
     }
 });
 
@@ -75,7 +75,7 @@ function chooseRandom() {
   var buttons = ['b1','b2','b3','b4'];
   var random = buttons[Math.floor(Math.random()*buttons.length)];
   computerPattern.push(random);
-  console.log(computerPattern);
+  console.log("computer generates a random number: "+computerPattern);
 }
 
 function computerTurn() {
@@ -91,18 +91,20 @@ function comparePattern() {
   for(var i=0;i<computerPattern.length; i++) {
     computerPatternCompare = computerPattern[i];
     playerPatternCompare = playerPattern[i];
+    console.log("playerPatternCompare = "+playerPatternCompare);
+    console.log("computerPatternCompare = "+computerPatternCompare)
     if (computerPattern.length > playerPattern.length) {
       console.log("player keeps playing");
     } else if (playerPatternCompare === computerPatternCompare) {
-      console.log("msg1: player pattern ="+playerPattern);
+      console.log("the numbers matched up "+"player number = "+playerPatternCompare+ "computer number = "+computerPatternCompare);
       playerTurns++;
       toggleTurn = 0;
       playerPattern = [];
       computerTurn();
     } else {
-      alert('game over. your score is '+ playerTurns);
+      alert("game over, your score is "+ playerTurns);
+      return;
     }
-  return;
   }
 }
 
