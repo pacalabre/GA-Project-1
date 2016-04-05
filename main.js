527569a8acc0bd1de1957eaace10df3bc5aca4bd
Paul <pacalabre@gmail.com>
2016-04-05 13:09:13 -0700

    playback is now good except for the newest pad being hit with the first pad on playback (when the pad is being hit) and at the end when it actually plays back

--- main.js
+++ main.js
@@ -6,76 +6,61 @@ var toggleTurn = 0;
 var playerTurns = 0;
 var computerPattern = [];
 var playerPattern = [];
-var i =0;
+var playInterval;
+var i = 0;
 
 //Button Assignments
 //Creating one function for each button - will reduce this once the game is working
 
 document.getElementById('b1').addEventListener("click", function(event) {
   event.preventDefault();
+  console.log(toggleTurn);
   if(toggleTurn === 0) {
-    setTimeout(cymbalSound(),2000);
+    cymbalSound();
     computerPattern.push('b1');
-    console.log("computer pattern ="+computerPattern);
-    console.log('toggleTurn = '+toggleTurn);
-    } else {
-      cymbalSound();
-      playerPattern.push('b1');
-      console.log("player plays back number = "+playerPattern);
-      console.log('toggleTurn = '+toggleTurn);
-      comparePattern();
-      console.log("player click");
-    }
+  } else {
+    cymbalSound();
+    playerPattern.push('b1');
+    comparePattern();
+  }
 });
 
 document.getElementById('b2').addEventListener("click", function(event) {
   event.preventDefault();
+  console.log(toggleTurn);
   if(toggleTurn === 0) {
-    setTimeout(highHatSound(),2000);
+    highHatSound();
     computerPattern.push('b2');
-    console.log("computer pattern ="+computerPattern);
-    console.log('toggleTurn = '+toggleTurn);
     } else {
       highHatSound();
       playerPattern.push('b2');
-      console.log("player plays back number = "+playerPattern);
-      console.log('toggleTurn = '+toggleTurn);
       comparePattern();
-      console.log("player click");
     }
 });
 
 document.getElementById('b3').addEventListener("click", function(event) {
   event.preventDefault();
+  console.log(toggleTurn);
   if(toggleTurn === 0) {
-    setTimeout(snareSound(),2000);
+    snareSound();
     computerPattern.push('b3');
-    console.log("computer pattern ="+computerPattern);
-    console.log('toggleTurn = '+toggleTurn);
     } else {
       snareSound();
       playerPattern.push('b3');
-      console.log("player plays back number = "+playerPattern);
-      console.log('toggleTurn = '+toggleTurn);
       comparePattern();
-      console.log("player click");
     }
 });
 
 document.getElementById('b4').addEventListener("click", function(event) {
   event.preventDefault();
+  console.log(toggleTurn);
   if(toggleTurn === 0) {
-    setTimeout(kickSound(),2000);
+    kickSound();
     computerPattern.push('b4');
-    console.log("computer pattern ="+computerPattern);
-    console.log('toggleTurn = '+toggleTurn);
     } else {
       kickSound();
       playerPattern.push('b4');
-      console.log("player plays back number = "+playerPattern);
-      console.log('toggleTurn = '+toggleTurn);
       comparePattern();
-      console.log("player click");
     }
 });
 
@@ -99,63 +84,87 @@ function chooseRandom() {
   //replace with vanilla JS event when working
 
   setTimeout(function() {
-    playComputerPattern();
     $('#'+random).trigger('click');
     toggleTurn = 1;
-}, 1000);
+  }, 1000);
   console.log('random ='+random)
   console.log("computer generates a random number: "+computerPattern);
   console.log('toggleTurn = '+ toggleTurn);
 
 }
 
-
 //new computer pattern
 
-function playComputerPattern() {
-  //Set interval one second apart from each other
-  //play back each button in var computerPattern
-  function myLoop () {           //  create a loop function
-     setTimeout(function () {    //  call a 3s setTimeout when the loop is called
+  function playComputerPattern() {
+    console.log("in playComputerPattern, computerPattern = "+ computerPattern);           //  create a loop function
 
-      if(computerPattern.length ===1){
-        console.log("first turn");
+    var j = 0;
+
+    playInterval = setInterval(function() {
+
+      if(j === computerPattern.length) {
+        clearInterval(playInterval);
       }
-      if (computerPattern.length > 1 && computerPattern[i] === 'b1') {
-        cymbalSound();
-        console.log('this');
 
-      } else if(computerPattern.length > 1 && computerPattern[i] === 'b2') {
+      if (computerPattern[j] === 'b1') {
+        cymbalSound();
+      } else if(computerPattern[j] == 'b2') {
         highHatSound();
         console.log('this2');
 
-      } else if(computerPattern.length > 1 && computerPattern[i] === 'b3') {
+      } else if(computerPattern[j] == 'b3') {
         snareSound();
         console.log('this3');
 
-      } else if (computerPattern.length > 1 && computerPattern[i] === 'b4') {
+      } else if (computerPattern[j] == 'b4') {
         kickSound();
         console.log('this4');
 
       } else {
         console.log("none of these");
+        console.log(computerPattern.length);
       }
-          i++;        //  increment the counter
-    if (i < computerPattern.length) {            //  if the counter < 10, call the loop function
-      myLoop();             //  ..  again which will trigger another
-    }                        //  ..  setTimeout()
-   }, 1000)
-  }
-  myLoop();
+
+      j++
+    }, 1000);
+
+  //    setTimeout(function () {    //  call a 3s setTimeout when the loop is called
+
+  //     //console.log(computerPattern[i]);
+
+  //     if (computerPattern[i] == 'b1') {
+  //       cymbalSound();
+  //       console.log('this');
+
+  //     } else if(computerPattern[i] == 'b2') {
+  //       highHatSound();
+  //       console.log('this2');
+
+  //     } else if(computerPattern[i] == 'b3') {
+  //       snareSound();
+  //       console.log('this3');
+
+  //     } else if (computerPattern[i] == 'b4') {
+  //       kickSound();
+  //       console.log('this4');
+
+  //     } else {
+  //       console.log("none of these");
+  //       console.log(computerPattern.length);
+  //     }
+
+  //     i++; //  increment the counter
+
+  //     if (i < computerPattern.length) {            //  if the counter < 10, call the loop function
+  //       playComputerPattern();             //  ..  again which will trigger another
+  //     }                        //  ..  setTimeout()
+  // }, 1000)
 }
-
-
-
 
 function computerTurn() {
   // var randomSelection = chooseRandom();
     toggleTurn = 0;
-    // playComputerPattern();
+    playComputerPattern();
     chooseRandom();
     // return toggleTurn=1;
 }
@@ -177,7 +186,6 @@ function comparePattern() {
     console.log("the numbers matched up "+"player number = "+computerPatternCompare+ "computer number = "+playerPatternCompare);
     playerTurns++;
     console.log(playerTurns);
-
     toggleTurn = 1;
 
     playerPattern = [];
